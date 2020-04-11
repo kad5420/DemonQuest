bfdd27f2f27a8ee1152ea9dc7d9430c282501b88
Kody <kodydeda4@gmail.com>
2020-04-11 10:28:36 -0400

    SpawnManager spawns EnemyPrefabs at each of its children transform's

---
+++ Assets/SpawnManager/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    // Start is called before the first frame update
+    List<Transform> spawnLocations;
+
+    void Start()
+    {
+        spawnLocations = transform.Cast<Transform>().ToList();
+        spawnEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnEnemies()
+    {
+        foreach (Transform location in spawnLocations)
+        {
+            Debug.Log(location);
+            Instantiate(enemyPrefab, location);
+        }
+    }
+}

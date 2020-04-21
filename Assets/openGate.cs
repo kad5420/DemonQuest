f75dbbafe7e95876fc1e0a2ff350e7c916dd9814
Kody Deda <kodydeda4@gmail.com>
2020-04-21 17:00:02 -0400

    Gate opens if character has weapons equipped

---
+++ Assets/openGate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class openGate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    public Invector.vMelee.vMeleeManager meleeManager;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((meleeManager.leftWeapon != null) && (meleeManager.rightWeapon != null))
+        {
+            Debug.Log("Opening Gate!!!");
+            anim.SetBool("openGate", true);
+        }
+    }
+}

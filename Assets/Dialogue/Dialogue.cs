7146021b48d2e9c820ccedf818ed3d965540dd11
Kody Deda <kodydeda4@gmail.com>
2020-04-21 17:49:20 -0400

    Paladin tells you to grab sword and shield

---
+++ Assets/Dialogue/Dialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator dialogue;
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("Hello world");
+        dialogue.SetBool("startDialogue", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

928d4b5c805f4c6a462cb98fb7ad2fe74efcdfcd
Kody Deda <kodydeda4@gmail.com>
2020-04-24 16:35:08 -0400

    Created WaveManager

---
+++ Assets/WaveManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpawnManager spawnManager;
+    public bool startWave = false;
+    public int currentWave = 1;
+    void Start()
+    {
+        spawnManager = this.gameObject.transform.GetChild(0).GetComponent<SpawnManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startWave)
+        {
+            spawnManager.spawnEnemyWave(currentWave);
+        }
+        startWave = false;
+    }
+}
